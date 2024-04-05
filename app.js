b34d00463ec1d1e042ca84ecfabab66c174f336d
chankara09 <laurize_wangkay@dlsu.edu.ph>
2024-04-05 17:46:42 +0800

    Revert "clickable edit"
    This reverts commit 142570faeace4bca043b3c8c8c5fc38f534b8598.

--- app.js
+++ app.js
@@ -917,7 +917,7 @@ server.post('/editReservationStudent', function(req, resp) {
         reservationModel.findOne(reservationSearchQuery).lean().then(function (reservation) {
             console.log(req.body.username);
             resp.render('editReservationStudent', {
-                layout: 'layoutReserve',
+                layout: 'index',
                 title: 'ILABS | Edit Reservation',
                 css: 'reserveStyle.css',
                 username: reservation.user,
@@ -930,7 +930,8 @@ server.post('/editReservationStudent', function(req, resp) {
                 ogtime: reservation.time_slot,
                 date: reservation.date,
                 ogdate: reservation.date,
-                'seat-data': seat_data,
+                'seat-data': seat_data
+                
             });
             console.log(req.body); 
         }).catch(errorFn);
